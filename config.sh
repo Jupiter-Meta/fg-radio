ad014e03c7cf122a231226ab89cd07fec2459833
nidhinmahesh <nidhinmahesh1@gmail.com>
2025-04-25 14:54:02 +0530

    updated prosody conf

--- config.sh
+++ config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Generate self-signed SSL certificates if needed
 if [ ! -f /etc/jitsi/meet/$XMPP_DOMAIN.crt ]; then
     echo "Generating self-signed SSL certificate for $XMPP_DOMAIN"
